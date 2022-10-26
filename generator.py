d140795d333ae5f0f7ec217128c00b57bcbbbe2f
Saranthyr <kirillmaksimets@gmail.com>
2022-10-26 17:42:04 +0300

    init commit

---
+++ generator.py
@@ -0,0 +1,28 @@
+import hashlib
+import random
+
+
+def array_gen(size, seed, limit, iteration):
+    arr = []
+    if iteration:
+        seed += hashlib.sha256(bytes(str(iteration), 'utf-8')).hexdigest()
+    random.seed(bytes(seed, 'utf-8'))
+    zeroes = 100 // random.randint(10, 40)
+    for i in range(size):
+        number = random.randint(-limit, limit)
+        chance = random.randint(0, 100) % zeroes
+        if chance == 0:
+            number = 0
+        arr.append(number)
+    return arr
+
+
+def builder(string, size: int, limit: int, task_number = None):
+    matrix = []
+    string += str(limit) + str(size)
+    if task_number:
+        string += str(task_number)
+    for i in range(size):
+        seed = hashlib.sha256(bytes(string, 'utf-8')).hexdigest()
+        matrix.append(array_gen(size, seed, limit, i))
+    return matrix
---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import generator
+
+
+app = Flask(__name__)
+
+
+@app.route('/generate1')
+def generate1():
+    namegroup = request.form.get('input')
+    size = int(request.form.get('size'))
+    limit = int(request.form.get('limit'))
+
+    matrix = generator.builder(namegroup, size, limit)
+
+    return jsonify({'matrix': matrix})
+
+@app.route('/generate2')
+def generate2():
+    namegroup = request.form.get('input')
+    size = int(request.form.get('size'))
+    limit = int(request.form.get('limit'))
+    task = int(request.form.get('task_number'))
+
+    matrix = generator.builder(namegroup, size, limit, task)
+
+    return jsonify({'matrix': matrix})
