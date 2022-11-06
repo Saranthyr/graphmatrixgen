fd4e096dac65066a0b37871760fc3ecbf4075294
Saranthyr <kirillmaksimets@gmail.com>
2022-11-07 00:50:04 +0300

    continue

--- app.py
+++ app.py
@@ -6,16 +6,6 @@ import generator
 import re
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    f'mysql+pymysql://std_1875_graphmatrixgen:Vjt_gjxntybt1@std-mysql/std_1875_graphmatrixgen?charset=utf8'
-app.config['NAMING_CONVENTION'] = {
-    'pk': 'pk_%(table_name)s',
-    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
-    'ix': 'ix_%(table_name)s_%(column_0_name)s',
-    'uq': 'uq_%(table_name)s_%(column_0_name)s',
-    'ck': 'ck_%(table_name)s_%(constraint_name)s',
-}
-db = flask_sqlalchemy.SQLAlchemy(app, metadata=MetaData(naming_convention=app.config['NAMING_CONVENTION']))
 
 
 @app.route('/')
@@ -23,38 +13,16 @@ def generic():
     return jsonify({'hi there'})
 
 
-@app.route('/generate_without_task_number', methods=['POST'])
-def generate_base():
-    namegroup = request.form.get('namegroup')
-    size = int(request.form.get('size'))
-    limit = int(request.form.get('limit'))
-    if re.fullmatch('[А-Яа-я0-9-]+', namegroup):
-        matrix = generator.builder_hashlib(namegroup.lower(), size, limit)
-        return jsonify({'matrix': matrix})
-    else:
-        return jsonify({'msg': 'incorrect name-group string'})
-
-
 @app.route('/generate_with_task_number', methods=['POST'])
-def generate_advanced():
-    namegroup = request.form.get('namegroup')
+def generate_base():
+    namegroup = request.form.get('input')
     size = int(request.form.get('size'))
-    limit = int(request.form.get('limit'))
-    task = int(request.form.get('task_number'))
+    negatives = request.form.get('negatives', None)
     if re.fullmatch('[А-Яа-я0-9-]+', namegroup):
-        matrix = generator.builder_hashlib(namegroup.lower(), size, limit, task)
-        return jsonify({'matrix': matrix})
-    else:
-        return jsonify({'msg': 'incorrect name-group string'})
-
-
-@app.route('/generate_with_saving_into_db', methods=['POST'])
-def generate_db():
-    namegroup = request.form.get('namegroup')
-    size = int(request.form.get('size'))
-    limit = int(request.form.get('limit'))
-    if re.fullmatch('[А-Яа-я0-9-]+', namegroup):
-        matrix = generator.builder_werkzeug(namegroup, size, limit)
+        if negatives:
+            matrix = generator.builder_hashlib(namegroup, size, negatives)
+        else:
+            matrix = generator.builder_hashlib(namegroup, size)
         return jsonify({'matrix': matrix})
     else:
         return jsonify({'msg': 'incorrect name-group string'})
--- generator.py
+++ generator.py
@@ -1,28 +1,23 @@
 import hashlib
 import random
-import uuid
-
-from sqlalchemy import and_
-from werkzeug.security import generate_password_hash
-from models import Students, MatrixParams
 
 
-def array_gen_hashlib(size, seed, limit, iteration, matrix):
+def array_gen_hashlib(size, seed, iteration, matrix):
     arr = []
     seed += hashlib.sha256(bytes(str(iteration), 'utf-8')).hexdigest()
     random.seed(bytes(seed, 'utf-8'))
-    zeroes = 100 // random.randint(25, 40)
+    zeroes = 100 // random.randint(15, 25)
     for i in range(size):
         if matrix:
             if i < iteration:
                 number = matrix[i][iteration]
             else:
-                number = random.randint(-limit, limit)
+                number = random.randint(0, 20)
                 chance = random.randint(0, 100) % zeroes
                 if chance == 0:
                     number = 0
         else:
-            number = random.randint(-limit, limit)
+            number = random.randint(0, 20)
             chance = random.randint(0, 100) % zeroes
             if chance == 0:
                 number = 0
@@ -30,79 +25,28 @@ def array_gen_hashlib(size, seed, limit, iteration, matrix):
     return arr
 
 
-def builder_hashlib(string, size: int, limit: int, task_number=None):
-    matrix = []
-    string += str(limit) + str(size)
-    string = hashlib.sha256(bytes(string, 'utf-8')).hexdigest()
-    string += hashlib.sha256(bytes(str(task_number), 'utf-8')).hexdigest()
-    for i in range(size):
-        seed = hashlib.sha256(bytes(string, 'utf-8')).hexdigest()
-        matrix.append(array_gen_hashlib(size, seed, limit, i, matrix))
+def matrix_checker(matrix, negatives):
+    min = 5
+    for i in range(negatives):
+        for j in range(len(matrix)):
+            for k in range(len(matrix)):
+                if 0 < matrix[j][k] < min:
+                    matrix[j][k] *= -1
+                    matrix[k][j] *= -1
+                    min = 5
     return matrix
 
 
-def array_gen_werkzeug(size, seed, limit):
-    arr = []
-    random.seed(bytes(seed, 'utf-8'))
-    zeroes = 100 // random.randint(10, 40)
-    for i in range(size):
-        number = random.randint(-limit, limit)
-        chance = random.randint(0, 100) % zeroes
-        if chance == 0:
-            number = 0
-        arr.append(number)
-    return arr
-
-
-def builder_werkzeug(string, size: int, limit: int):
-    from app import db
+def builder_hashlib(string, size: int, negatives=None):
     matrix = []
-    student = db.session.query(Students).filter(Students.student_name_group.ilike(string)).one_or_none()
-    if student:
-        student_matrix_params = db.session.query(MatrixParams).filter(and_(MatrixParams.student_id == student.id,
-                                                                           MatrixParams.size == size)).one_or_none()
-        if student_matrix_params:
-            size = student_matrix_params.size
-            limit = student_matrix_params.matrix_limit
-            seeds = student_matrix_params.seeds.split(';')
-            for i in range(len(seeds)):
-                matrix.append(array_gen_werkzeug(size, seeds[i], limit))
-        else:
-            seeds = ''
-            for i in range(size):
-                seed = generate_password_hash(string, 'sha256', 64)
-                seeds += seed + ';'
-                matrix.append(array_gen_werkzeug(size, seed, limit))
-            matrix_params = MatrixParams(
-                id=str(uuid.uuid4()),
-                student_id=student.id,
-                size=size,
-                matrix_limit=limit,
-                seeds=seeds
-            )
-            db.session.add(matrix_params)
-            db.session.flush()
-            db.session.commit()
-    else:
-        stud_id = str(uuid.uuid4())
-        student = Students(
-            id=stud_id,
-            student_name_group=string
-        )
-        db.session.add(student)
-        seeds = ''
-        for i in range(size):
-            seed = generate_password_hash(string, 'sha256', 64)
-            seeds += seed + ';'
-            matrix.append(array_gen_werkzeug(size, seed, limit))
-        matrix_params = MatrixParams(
-            id=str(uuid.uuid4()),
-            student_id=stud_id,
-            size=size,
-            matrix_limit=limit,
-            seeds=seeds
-        )
-        db.session.add(matrix_params)
-        db.session.flush()
-        db.session.commit()
+    string += str(size)
+    string = hashlib.sha256(bytes(string, 'utf-8')).hexdigest()
+    for i in range(size):
+        seed = hashlib.sha256(bytes(string, 'utf-8')).hexdigest()
+        matrix.append(array_gen_hashlib(size, seed, i, matrix))
+    if negatives:
+        random.seed(string)
+        negatives = random.randint(2, 5)
+        print(negatives)
+        matrix = matrix_checker(matrix, negatives)
     return matrix
